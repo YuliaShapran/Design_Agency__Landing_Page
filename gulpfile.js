3b021e77d494a345a158fe3125361f81ec15d97f
Yulia <yuliashapran1993@gmail.com>
2021-09-24 12:56:55 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+
+gulp.task('server', function () {
+
+	browserSync({
+		server: {
+			baseDir: "src"
+		}
+	});
+
+	gulp.watch("src/*.html").on('change', browserSync.reload);
+
+	gulp.watch('src/img/*');
+});
+
+gulp.task('styles', function () {
+	return gulp.src("src/sass/**/*.+(scss|sass)")
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(rename({ suffix: '.min', prefix: '' }))
+		.pipe(autoprefixer())
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(gulp.dest("src/css"))
+		.pipe(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.mozjpeg({ quality: 75, progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
+		.pipe(gulp.dest('dist/img'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+	gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
